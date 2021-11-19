526590c87d6cf65b4f2355b9c4dfde728feb2260
Addo.Zhang <duwasai@gmail.com>
2021-11-19 19:58:56 +0800

    update scripts to init infra repo and pause repo

--- init-infra-repo.sh
+++ init-infra-repo.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -exu
-#### local use
-# REPO_HOST=localhost:6060
-# REPO_NAME=infra
-#### remote repo
-REPO_HOST=hub.flomesh.cn:6060
-REPO_NAME=pod/$1
+#### local repo
+REPO_HOST=localhost:6060
+REPO_NAME=pipy-infra
+
+version=`curl -s http://$REPO_HOST/api/v1/repo/$REPO_NAME | jq -r .version`
+version=$(( version+1 ))
 
 #create repo
 curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME
@@ -15,12 +15,10 @@ curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/main.js --data-binary '@./
 #config
 curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/config/eureka.json --data-binary '@./scripts/config/eureka.json'
 curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/config/main.json --data-binary '@./scripts/config/main.json'
-curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/config/pod.json --data-binary '@./scripts/config/pod.json'
 #plugins
 curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/plugins/log.js --data-binary '@./scripts/plugins/log.js'
 curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/plugins/registry.js --data-binary '@./scripts/plugins/registry.js'
 curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME/plugins/proxy.js --data-binary '@./scripts/plugins/proxy.js'
 
 #release
-curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME --data '{"version": 2}'
-
+curl -X POST http://$REPO_HOST/api/v1/repo/$REPO_NAME --data '{"version": '$version'}'
